5d55b3ac6e4203c1960d379b1173527b4bfcbb84
Yiannis Kazantzidis <136897071+yiannis-kazantzidis@users.noreply.github.com>
2024-09-01 01:25:58 +0100

    need to deploy

--- src/errors.ts
+++
@@ -1,7 +0,0 @@
-export class SessionExpired extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = 'SessionExpired';
-    Error.captureStackTrace(this, SessionExpired)
-  }
-}
